832aeb321b53ec86bdbd73dde732971c049e73cf
Raskolnikov <jerry.tann@endress.com>
2022-04-28 18:08:10 +0800

    📝 more setup

---
+++ index.js
@@ -0,0 +1,43 @@
+function handleContents(contents) {
+    splitlines = contents.split('\r\n')
+    console.log('split lines is ', splitlines)
+}
+
+
+function readSingleFile(e) {
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    handleContents(contents)
+  };
+  reader.readAsText(file);
+}
+
+
+document.querySelector("#file-input").addEventListener('change', function(ev) {
+    console.log('ev is ', ev)
+    readSingleFile(ev)
+})
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  }
+
+document.querySelector('#convert').addEventListener('click', function(ev) {
+    console.log('will create file ')
+    download('result.csv', 'some,text,bro\nmore,text,bro')
+})
